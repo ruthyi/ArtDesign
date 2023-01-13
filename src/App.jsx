5226bfad7affcc899856291f3622293876ea32dd
ruthyi <jbruthlizcano@gmail.com>
2023-01-13 14:45:17 -0500

    Tidy folder

--- src/App.jsx
+++ src/App.jsx
@@ -17,6 +17,7 @@ function App() {
       </div>
       <h1>Vite + React</h1>
       <div className="card">
+        <footer></footer>
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
         </button>
