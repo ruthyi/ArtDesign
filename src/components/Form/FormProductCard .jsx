c7da13b20f768fed3b27778af7ed9a32e6df3c2d
ruthyi <jbruthlizcano@gmail.com>
2023-01-19 10:41:00 -0500

    create component form

--- src/components/Form/FormProductCard .jsx
+++ src/components/Form/FormProductCard .jsx
@@ -1,21 +1,35 @@
 import React from 'react'
+import { useState } from 'react';
 import { Form, Formik } from 'formik';
 import { InputsField } from './InputsField ';
+import styleForm from './FormProductCard.module.css'
 
 export const FormProductCard = (props) => {
-    const {price,detail}=props;
+    const { price, detail } = props;
+    const [lastPrice, setListado] = useState(price)
     return (
         <div>
             <Formik initialValues={{
-                    name: '',
-                    price: '',
-                }}  onSubmit={(values, { resetForm }) => {
-                    console.log(values)
-                    resetForm()}} >
+                name: '',
+                price: '',
+            }} onSubmit={(values, { resetForm }) => {
+                console.log(values)
+                resetForm()
+            }} >
                 {formik => (
                     <Form>
                         <InputsField label="" name="name" type="text" placeholder={detail} />
-                        <InputsField  name="price" type="text" placeholder="0" />
+                        <section>
+                            <div>
+                                <button>-</button>
+                                <InputsField name="price" type="text" placeholder="1" />
+                                <button>+</button>
+                            </div>
+                            <div>
+                                <h1>$</h1>
+                                <h1>{lastPrice}</h1>
+                            </div>
+                        </section>
                     </Form>
                 )}
             </Formik>
