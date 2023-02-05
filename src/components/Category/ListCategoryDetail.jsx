809353594e1ad4588392a7162413c48e4707f4a2
ruthyi <jbruthlizcano@gmail.com>
2023-02-05 18:00:02 -0500

    add pagination

--- src/components/Category/ListCategoryDetail.jsx
+++ src/components/Category/ListCategoryDetail.jsx
@@ -1,39 +1,78 @@
-import React from 'react'
+import React, { useEffect, useState } from "react";
 import { dataDetail } from '../../Data';
 import { CategoryCards } from './CategoryCards';
 import style from './Category.module.css'
+import ReactDOM from "react-dom";
+import ReactPaginate from "react-paginate";
+
 
 export const ListCategoryDetail = (props) => {
   const { nameFilter } = props;
+  const itemsPerPage = 8;
+  const [itemOffset, setItemOffset] = useState(0);
 
+  let currentItems;
+  let pageCount;
+  
+
+  function filtracion(filtro) {
+    if (filtro == "all") {
+      currentItems = dataDetail.slice(itemOffset, endOffset);
+      pageCount = Math.ceil(Object.values(dataDetail).length / itemsPerPage);
+    } else {
+      let res = [];
+      dataDetail.map((lista) => {
+        if (lista.filter == filtro) {
+          res.push(lista);
+        }
+      });
+      currentItems = res.slice(itemOffset, endOffset);
+      pageCount = Math.ceil(Object.values(res).length / itemsPerPage);
+    }
+  }
+  const endOffset = itemOffset + itemsPerPage;
+  console.log(`Loading items from ${itemOffset} to ${endOffset}`);
+
+  if (nameFilter == "all") {
+    filtracion("all");
+  } else if (nameFilter == "detail") {
+    filtracion("detail");
+  } else{
+    filtracion("boxesAndPackaging");
+  }
+
+  // Invoke when user click to request another page.
+  const handlePageClick = (event) => {
+    const newOffset =
+      (event.selected * itemsPerPage) % Object.values(dataDetail).length;
+    console.log(
+      `User requested page number ${event.selected}, which is offset ${newOffset}`
+    );
+    setItemOffset(newOffset);
+  };
   return (
-    <section className={style.containerCards}>
+    <section className={style.sectioncontainerCards}>
+      <div className={style.containerCards}>
+        {}
+        {currentItems.map((lista) => {
+          return (
+            <div key={lista.id}>
+              <CategoryCards lista={lista} />
+            </div>
+          );
+        })}
+      </div>
       {
-        dataDetail.map((lista => {
-          if (nameFilter == "all") {
-            return (
-              <div key={lista.id}>
-                <CategoryCards lista={lista} />
-              </div>
-            )
-          } else if (nameFilter == "detail" && lista.filter == "detail") {
-            return (
-              <div key={lista.id}>
-                <CategoryCards lista={lista} />
-              </div>
-            )
-          } else if (nameFilter == "boxesAndPackaging" && lista.filter == "boxesAndPackaging") {
-            return (
-              <div key={lista.id}>
-                <CategoryCards lista={lista} />
-              </div>
-            )
-          } 
-
-        }))
+        <ReactPaginate
+          breakLabel="..."
+          nextLabel=">"
+          onPageChange={handlePageClick}
+          pageRangeDisplayed={4}
+          pageCount={pageCount}
+          previousLabel="<"
+          renderOnZeroPageCount={null}
+        />
       }
-
     </section>
-
-  )
+  );
 }
--- src/components/Category/ListCategorySublimation.jsx
+++ src/components/Category/ListCategorySublimation.jsx
@@ -1,45 +1,79 @@
-import React from 'react'
+import React, { useEffect, useState } from "react";
 import { dataSublimationPrints } from '../../Data';
 import { CategoryCards } from './CategoryCards';
 import style from './Category.module.css'
-
+import ReactDOM from "react-dom";
+import ReactPaginate from "react-paginate";
 export const ListCategorySublimation = (props) => {
   const { nameFilter } = props;
+  const itemsPerPage = 8;
+  const [itemOffset, setItemOffset] = useState(0);
+  let currentItems;
+  let pageCount;
 
+  function filtracion(filtro) {
+    if (filtro == "all") {
+      currentItems = dataSublimationPrints.slice(itemOffset, endOffset);
+      pageCount = Math.ceil(Object.values(dataSublimationPrints).length / itemsPerPage);
+    } else {
+      let res = [];
+      dataSublimationPrints.map((lista) => {
+        if (lista.filter == filtro) {
+          res.push(lista);
+        }
+      });
+      currentItems = res.slice(itemOffset, endOffset);
+      pageCount = Math.ceil(Object.values(res).length / itemsPerPage);
+    }
+  }
+  // Simulate fetching items from another resources.
+  // (This could be items from props; or items loaded in a local state
+  // from an API endpoint with useEffect and useState)
+  const endOffset = itemOffset + itemsPerPage;
+  console.log(`Loading items from ${itemOffset} to ${endOffset}`);
+
+  if (nameFilter == "all") {
+    filtracion("all");
+  } else if (nameFilter == "caps") {
+    filtracion("caps");
+  } else if (nameFilter== "tshirts") {
+    filtracion("tshirts");
+  }else {
+    filtracion("crockery");
+  }
+
+  // Invoke when user click to request another page.
+  const handlePageClick = (event) => {
+    const newOffset =
+      (event.selected * itemsPerPage) % Object.values(dataSublimationPrints).length;
+    console.log(
+      `User requested page number ${event.selected}, which is offset ${newOffset}`
+    );
+    setItemOffset(newOffset);
+  };
   return (
-    <section className={style.containerCards}>
+    <section className={style.sectioncontainerCards}>
+      <div className={style.containerCards}>
+        {}
+        {currentItems.map((lista) => {
+          return (
+            <div key={lista.id}>
+              <CategoryCards lista={lista} />
+            </div>
+          );
+        })}
+      </div>
       {
-        dataSublimationPrints.map((lista => {
-          if (nameFilter == "all") {
-            return (
-              <div key={lista.id}>
-                <CategoryCards lista={lista} />
-              </div>
-            )
-          } else if (nameFilter == "caps" && lista.filter == "caps") {
-            return (
-              <div key={lista.id}>
-                <CategoryCards lista={lista} />
-              </div>
-            )
-          } else if (nameFilter == "tshirts" && lista.filter == "tshirts") {
-            return (
-              <div key={lista.id}>
-                <CategoryCards lista={lista} />
-              </div>
-            )
-          } else if (nameFilter == "crockery" && lista.filter == "crockery") {
-            return (
-              <div key={lista.id}>
-                <CategoryCards lista={lista} />
-              </div>
-            )
-          } 
-
-        }))
+        <ReactPaginate
+          breakLabel="..."
+          nextLabel=">"
+          onPageChange={handlePageClick}
+          pageRangeDisplayed={4}
+          pageCount={pageCount}
+          previousLabel="<"
+          renderOnZeroPageCount={null}
+        />
       }
-
     </section>
-
-  )
+  );
 }
